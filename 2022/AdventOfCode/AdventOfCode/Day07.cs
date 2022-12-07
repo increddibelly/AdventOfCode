0bbda444ecf01df877c849a1862ae99e65fd5926
Joost Boomkamp <increddibelly@gmail.com>
2022-12-07 08:46:46 +0100

    Day 7

--- 2022/AdventOfCode/AdventOfCode/Day07.cs
+++ 2022/AdventOfCode/AdventOfCode/Day07.cs
@@ -1,35 +1,131 @@
-﻿namespace AdventOfCode;
+﻿using System.Collections.Immutable;
+using System.Drawing;
+
+namespace AdventOfCode;
 
 public class Day07
 {
-    public static T[] Parse(string input)
-    {
+    private Dictionary<string, Dictionary<string, int>> _fileSystem = new Dictionary<string, Dictionary<string, int>>();
+    private Dictionary<string, int> _folderSizes = new Dictionary<string, int>();
 
+    public void Parse(string input)
+    {
+        var lines = input.Replace("/", "\\").Split(Environment.NewLine);
+        PrepareFileSystem(lines);
     }
 
-    public string Day07Part1(string input)
+    private int RecursiveFolderSize(string path)
     {
-        var items = Parse(input);
+        return _folderSizes.Where(x => x.Key.Contains(path)).Sum(x => x.Value);
+    }
 
-        foreach (var item in items)
+    private void PrepareFileSystem(string[] lines)
+    {
+        var currentPath = Path.GetFullPath("\\");
+        var filesInCurrentFolder = new Dictionary<string, int>();
+
+        foreach (var line in lines)
         {
+            // command
+            var cmd = line.Split(' ').ToArray();
+            switch (cmd[0])
+            {
+                case "$":
+                    switch (cmd[1])
+                    {
+                        case "cd":
+                            // consolidate current folder
+                            ConsolidateCurrentPath(currentPath, filesInCurrentFolder);
 
+                            if (cmd[2] == "..")
+                            {
+                                currentPath = Directory.GetParent(currentPath).FullName;
+                            }
+                            else
+                            {
+                                currentPath = Path.GetFullPath(Path.Combine(currentPath, cmd[2]));
+                            }
+                            break;
+                        case "ls": break;
+                    }
+                    break;
 
+                default:
+                    // this is a file listing
+                    if (cmd[0].StartsWith("dir"))
+                    {
+                        continue;
+                    }
+                    filesInCurrentFolder.Add(cmd[1], int.Parse(cmd[0]));
+                    break;
+            }
         }
 
-        return
+        if (filesInCurrentFolder.Any())
+        {
+            ConsolidateCurrentPath(currentPath, filesInCurrentFolder);
+        }
     }
 
-    public string Day07Part2(string input)
+    private void ConsolidateCurrentPath(string currentPath, Dictionary<string, int> filesInCurrentFolder)
     {
-        var items = Parse(input);
-
-        foreach (var item in items)
+        if (!_fileSystem.ContainsKey(currentPath))
         {
-
-
+            _fileSystem.Add(currentPath, new Dictionary<string, int>());
+            _folderSizes.Add(currentPath, 0);
         }
 
-        return
+        foreach (var file in filesInCurrentFolder)
+        {
+            _fileSystem[currentPath].Add(file.Key, file.Value);
+        }
+
+        // calculate ONLY the current folder size
+        _folderSizes[currentPath] = _fileSystem[currentPath].Sum(x => x.Value);
+
+        filesInCurrentFolder.Clear(); // current folder means something else now
     }
+
+    public int Day07Part1()
+    {
+        var absoluteSizes = new Dictionary<string, int>();
+        foreach (var item in _fileSystem)
+        {
+            var size = RecursiveFolderSize(item.Key);
+            absoluteSizes.Add(item.Key, size);
+        }
+
+        return absoluteSizes.Select(x => x.Value).Where(x => x <= 100000).Sum();
+    }
+
+    public int Day07Part2()
+    {
+        var totalSize = new Dictionary<string, int>();
+        foreach (var item in _fileSystem)
+        {
+            var size = RecursiveFolderSize(item.Key);
+            totalSize.Add(item.Key, size);
+        }
+        totalSize = totalSize.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+        var freeSpace = 70000000 - totalSize.Last().Value;
+        var target = 30000000 - freeSpace;
+
+        var smallestOver = totalSize.Values.Where(folder => folder >= target).Min();
+
+        return smallestOver;
+    }
+
+    //public string Day07Part2(string input)
+    //{
+    //    var items = Parse(input);
+
+    //    foreach (var item in items)
+    //    {
+
+
+    //    }
+
+    //    return
+    //}
 }
--- 2022/AdventOfCode/Tests/Tests07.cs
+++ 2022/AdventOfCode/Tests/Tests07.cs
@@ -4,42 +4,43 @@ namespace Tests;
 
 public class Tests07
 {
+    [Test]
     public void Day07Part1_Example()
     {
         var Day07 = new Day07();
-        var items = Day07.Parse(Input.Day07Example);
+        Day07.Parse(Input.Day07Example);
 
-        var result = Day07.Day07Part1(items);
-        result.Should().Be(1337);
+        var result = Day07.Day07Part1();
+        result.Should().Be(95437);
     }
 
-    //[Test]
-    //public void Day07Part1()
-    //{
-    //    var Day07 = new Day07();
-    //    var items = Day07.Parse(Input.Day07Input);
+    [Test]
+    public void Day07Part1()
+    {
+        var Day07 = new Day07();
+        Day07.Parse(Input.Day07Input);
 
-    //    var result = Day07.Day07Part1(items);
-    //    result.Should().Be(1337);
-    //}
+        var result = Day07.Day07Part1();
+        result.Should().Be(1770595);
+    }
 
+    [Test]
+    public void Day07Part2_Example()
+    {
+        var Day07 = new Day07();
+        Day07.Parse(Input.Day07Example);
 
-    //public void Day07Part1_Example()
-    //{
-    //    var Day07 = new Day07();
-    //    var items = Day07.Parse(Input.Day7Example);
+        var result = Day07.Day07Part2();
+        result.Should().Be(24933642);
+    }
 
-    //    var result = Day07.Day07Part2(items);
-    //    result.Should().Be(1337);
-    //}
+    [Test]
+    public void Day07Part2()
+    {
+        var Day07 = new Day07();
+        Day07.Parse(Input.Day07Input);
 
-    //[Test]
-    //public void Day07Part1()
-    //{
-    //    var Day07 = new Day07();
-    //    var items = Day07.Parse(Input.Day07Input);
-
-    //    var result = Day07.Day07Part2(items);
-    //    result.Should().Be(1337);
-    //}
+        var result = Day07.Day07Part2();
+        result.Should().Be(2195372);
+    }
 }
