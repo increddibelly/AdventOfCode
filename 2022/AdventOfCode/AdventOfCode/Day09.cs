3ef0db12c6afd7b3319ec23877919c2439f73496
Joost Boomkamp <increddibelly@gmail.com>
2022-12-10 08:01:49 +0100

    Day9

--- 2022/AdventOfCode/AdventOfCode/Day09.cs
+++ 2022/AdventOfCode/AdventOfCode/Day09.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,12 @@ namespace AdventOfCode
 {
     public class Day09
     {
+        Point T, H;
+
+        List<Point> knots = new List<Point>();
+
+        List<Point> visited = new List<Point>();
+
         public string[] Parse(string input)
         {
             return input.Split(Environment.NewLine);
@@ -15,12 +22,86 @@ namespace AdventOfCode
 
         public int Day09Part1(string[] input)
         {
-            return 0;
+            T = new Point(0, 0);
+            H = new Point(0, 0);
+            visited.Add(T);
+            foreach(var line in input)
+            {
+                var direction = line[0].ToString();
+                var steps = int.Parse(line.Substring(2));
+
+                for (var i = 0; i < steps; i++)
+                {
+                    Move(ref H, direction);
+                    Follow(H, ref T);
+                }
+            }
+            return visited.Distinct().Count();
+        }
+
+        private void Move(ref Point p, string direction)
+        {
+            switch(direction)
+            {
+                case "L" : p.X -= 1; break;
+                case "R" : p.X += 1; break;
+                case "U" : p.Y -= 1; break;
+                case "D" : p.Y += 1; break;
+            }
+        }
+        
+        private void Follow(Point head, ref Point tail, bool track = true)
+        {
+            var dx = head.X - tail.X;
+            var dy = head.Y - tail.Y;
+            if (Math.Abs(dx) > 1 || Math.Abs(dy) > 1)
+            {
+                dx = dx == 0 ? 0 : dx / Math.Abs(dx);
+                dy = dy == 0 ? 0 : dy / Math.Abs(dy);
+
+                tail.X += dx;
+                tail.Y += dy;
+
+                if (track)
+                {
+                    visited.Add(tail);
+                }
+            }
         }
 
         public int Day09Part2(string[] input)
         {
-            return 0;
+            var head = new Point(0, 0);
+            var h1 = new Point(0, 0);
+            var h2 = new Point(0, 0);
+            var h3 = new Point(0, 0);
+            var h4 = new Point(0, 0);
+            var h5 = new Point(0, 0);
+            var h6 = new Point(0, 0);
+            var h7 = new Point(0, 0);
+            var h8 = new Point(0, 0);
+            var tail = new Point(0, 0);
+            visited.Add(tail);
+            foreach (var line in input)
+            {
+                var direction = line[0].ToString();
+                var steps = int.Parse(line.Substring(2));
+
+                for (var i = 0; i < steps; i++)
+                {
+                    Move(ref head, direction);
+                    Follow(head, ref h1, false);
+                    Follow(h1, ref h2, false);
+                    Follow(h2, ref h3, false);
+                    Follow(h3, ref h4, false);
+                    Follow(h4, ref h5, false);
+                    Follow(h5, ref h6, false);
+                    Follow(h6, ref h7, false);
+                    Follow(h7, ref h8, false);
+                    Follow(h8, ref tail);
+                }
+            }
+            return visited.Distinct().Count();
         }
     }
 }
--- 2022/AdventOfCode/Tests/Tests09.cs
+++ 2022/AdventOfCode/Tests/Tests09.cs
@@ -1,45 +1,46 @@
-//using AdventOfCode;
+using AdventOfCode;
 
-//namespace Tests;
+namespace Tests;
 
-//public class Tests09
-//{
-//    public void Day09Part1_Example()
-//    {
-//        var Day09 = new Day09();
-//        var items = Day09.Parse(Input.Day09Example);
+public class Tests09
+{
+    [Test]
+    public void Day09Part1_Example()
+    {
+        var Day09 = new Day09();
+        var items = Day09.Parse(Input.Day09Example);
 
-//        var result = Day09.Day09Part1(items);
-//        result.Should().Be(1337);
-//    }
+        var result = Day09.Day09Part1(items);
+        result.Should().Be(13);
+    }
 
-//    //[Test]
-//    //public void Day09Part1()
-//    //{
-//    //    var Day09 = new Day09();
-//    //    var items = Day09.Parse(Input.Day09Input);
+    [Test]
+    public void Day09Part1()
+    {
+        var Day09 = new Day09();
+        var items = Day09.Parse(Input.Day09Input);
 
-//    //    var result = Day09.Day09Part1(items);
-//    //    result.Should().Be(1337);
-//    //}
+        var result = Day09.Day09Part1(items);
+        result.Should().Be(6256);
+    }
 
 
-//    //public void Day09Part1_Example()
-//    //{
-//    //    var Day09 = new Day09();
-//    //    var items = Day09.Parse(Input.Day7Example);
+    public void Day09Part2_Example()
+    {
+        var Day09 = new Day09();
+        var items = Day09.Parse(Input.Day09Example);
 
-//    //    var result = Day09.Day09Part2(items);
-//    //    result.Should().Be(1337);
-//    //}
+        var result = Day09.Day09Part2(items);
+        result.Should().Be(1337);
+    }
 
-//    //[Test]
-//    //public void Day09Part1()
-//    //{
-//    //    var Day09 = new Day09();
-//    //    var items = Day09.Parse(Input.Day09Input);
+    [Test]
+    public void Day09Part2()
+    {
+        var Day09 = new Day09();
+        var items = Day09.Parse(Input.Day09Input);
 
-//    //    var result = Day09.Day09Part2(items);
-//    //    result.Should().Be(1337);
-//    //}
-//}
+        var result = Day09.Day09Part2(items);
+        result.Should().Be(1337);
+    }
+}
