ee7a167d56eca8d19e1f7f8f03bfff675b679875
Joost Boomkamp <increddibelly@gmail.com>
2022-12-15 08:09:49 +0100

    15

--- 2022/AdventOfCode/AdventOfCode/Day15.cs
+++ 2022/AdventOfCode/AdventOfCode/Day15.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,57 @@ namespace AdventOfCode;
 
 public class Day15
 {
+    private Map<char> _map;
+
+    private Dictionary<Point, Point> _sensors = new Dictionary<Point, Point>();
+
     private void Parse(string input)
     {
+        // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        var lines = input
+            .Split(Environment.NewLine)
+            .Select(x => x.Replace("Sensor at x=", "")
+            .Replace("y=", "")
+            .Replace(" closest beacon is at ", "")
+            .Split(":"));
+        foreach(var line in lines)
+        {
+            _sensors.Add(
+                new Point(int.Parse(line[0]), int.Parse(line[1])),
+                new Point(int.Parse(line[2]), int.Parse(line[3]))
+                );
+        }
 
+        var minSX = _sensors.Keys.Min(x => x.X);
+        var minBX = _sensors.Values.Min(x => x.X);
+        var maxSX = _sensors.Keys.Max(x => x.X);
+        var maxBX = _sensors.Values.Max(x => x.X);
+
+        var minSY = _sensors.Keys.Min(x => x.Y);
+        var minBY = _sensors.Values.Min(x => x.Y);
+        var maxSY = _sensors.Keys.Max(x => x.Y);
+        var maxBY = _sensors.Values.Max(x => x.Y);
+
+        var xSize = Math.Max(maxBX, maxSX) - Math.Min(minBX, minSX);
+        var ySize = Math.Max(maxBY, maxSY) - Math.Min(minBY, minSY);
+
+        _map = new Map<char>(xSize, ySize, 0, 0, '.');
+        foreach(var s in _sensors)
+        {
+            //deltaX, delta Y = range
+            //blot out range
+        }
+    }
+
+    private bool BeaconBetween(Point a, Point b)
+    {
+        return _sensors.Values.Any(b => b.X )
     }
 
     public int Day15Part1(string input)
     {
-        return 0;
+        Parse(input);
+
     }
 
     public int Day15Part2(string input)
--- 2022/AdventOfCode/AdventOfCode/Input.cs
+++ 2022/AdventOfCode/AdventOfCode/Input.cs
@@ -10863,7 +10863,20 @@ abaccccccccccaccaaaacccaaaaaaaaaaaaaaccccccaaaaaccccccccccccccccccccccccccccaaaa
 502,138 -> 502,141 -> 498,141 -> 498,145 -> 514,145 -> 514,141 -> 507,141 -> 507,138
 486,52 -> 490,52";
 
-    public static string Day15Example = @"";
+    public static string Day15Example = @"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3";
     public static string Day15Input = @"";
 
 }
